9749212b79c7cb8f17755d92214f5122233c1278
NaitsabesMue <51112618+NaitsabesMue@users.noreply.github.com>
2025-10-23 16:09:10 +0200

    add: evaluation data

---
+++ data/eval_data_week4.py
@@ -0,0 +1,21 @@
+# Evaluation data for week 4 classification lab
+
+eval_data = [
+    {"text": "Quand est mon prochain cours?", "label": "get_schedule"},
+    {"text": "Je peux faire la fete ce soir, ou cous dmain matin?", "label": "get_schedule"},
+    {"text": "Quelle salle pour mon cours de 10h?", "label": "get_schedule"},
+    {"text": "Où est ma boite de réception?", "label": "check_email"},
+    {"text": "J'ai reçu un mel important?", "label": "check_email"},
+    {"text": "Je dois ecrire un mail à mon prof", "label": "check_email"},
+    {"text": "Je dois m'inscrire en L3", "label": "register_classes"},
+    {"text": "C'est quand les IP?", "label": "register_classes"},
+    {"text": "Comment faire pour s'inscrire aux cours?", "label": "register_classes"},
+    {"text": "Mince, j ai perdu ma crte étudiante", "label": "get_student_card"},
+    {"text": "Je veux charger ma carte étudiante", "label": "get_student_card"},
+    {"text": "Comment recuperer ma carte etudiante?", "label": "get_student_card"},
+    {"text": "Les horaires de la BU St Charles, c'est où?", "label": "find_library_info"},
+    {"text": "Comment réserver un livre à la BU d'AMU?", "label": "find_library_info"},
+    {"text": "Je dois rendnre en pret?", "label": "find_library_info"},
+    {"text": "Salut, ca va?", "label": "chat"},
+    {"text": "Le banana est plus jaune que la grenouille.", "label": "unknown"}
+]
